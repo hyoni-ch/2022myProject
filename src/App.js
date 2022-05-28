6f86b6551e1a53459ae71715861f664abdd4f3b2
Hahyeon Cho <asdfgh9933@daum.net>
2022-05-28 15:24:21 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className='form-wrapper'>
+        <input className="title-input" type='text' placeholder='제목' />
+      </div>
+      <button className="submit-button">입력</button>
+      
+    </div>
+  );
+}
+
+export default App;
